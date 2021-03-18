d723afa6cb6f7172f13bbdbbafdeddd8bcbf30bc
bebofofum <bri.boehm@gmail.com>
2021-03-17 20:45:06 -0400

    Added some styling

--- pet-feeder-client/src/components/PetList.js
+++ pet-feeder-client/src/components/PetList.js
@@ -4,8 +4,8 @@ import PetListItem from './PetListItem'
 const PetList = ({pets}) => {
     return(
         <div>
-            <h1>Pets</h1>
-            <ul>{pets.map(pet => <PetListItem key={pet.id} pet={pet} />)}</ul>
+            <h1 className="text-3xl font-semibold pb-4 text-green-700">Pets</h1>
+            <ul className="">{pets.map(pet => <PetListItem key={pet.id} pet={pet} />)}</ul>
         </div>
         
     )
--- pet-feeder-client/src/components/PetListItem.js
+++ pet-feeder-client/src/components/PetListItem.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const PetListItem = ({pet}) => {
     return(
-        <>
-            <li className="" >{pet.name}</li>
-            <li className="" >{pet.species}</li>
-        </>
+        <div className="bg-green-200 rounded-lg">
+            <li className="text-purple-600 capitalize text-xl font-bold m-2 px-3 pt-3" >{pet.name}</li>
+            <li className="text-gray-600 pb-3 m-2 px-3 mb-1" >{pet.species}</li>
+        </div>
     )
 
 }
