dd21c3f43fd5208c1b9d3cbd16f991bf96ba11d7
bebofofum <bri.boehm@gmail.com>
2021-04-16 20:37:27 -0400

    Adding delete feature to owner list

--- pet-feeder-client/src/components/OwnerListItem.js
+++ pet-feeder-client/src/components/OwnerListItem.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const OwnerListItem = ({owner}) => {
     return(
-        <div className="bg-gray-200 rounded-lg">
-            <li className="text-gray-600 text-l font-bold m-3 px-3 p-3" >{owner.name}</li>
+        <div className="max-w-xl w-full p-4 my-3 flex md:flex-row flex-col space-x-3 bg-gray-200 rounded-lg">
+            <div className="flex-1 pt-1">
+                <li className="font-semibold text-lg tracking-wide" >{owner.name}</li>
+            </div>
+            <div className="md:px-2 mt-3 md:mt-0 items-center flex">
+                <button className="bg-blue-500 text-white font-bold px-4 py-2 text-sm uppercase rounded tracking-wider focus:outline-none hover:bg-blue-600">DELETE OWNER</button>
+            </div>
         </div>
     )
 }
