e0360a14ab8fc65242ed132858bda87cd8b229f6
bebofofum <bri.boehm@gmail.com>
2021-06-19 00:19:07 -0400

    Refactored delete button. Did not need to be class component.

--- pet-feeder-client/src/components/NavBar.js
+++ pet-feeder-client/src/components/NavBar.js
@@ -18,27 +18,47 @@ class NavBar extends Component {
   renderAuthLinks() {  
     const { authCheck, loggedIn, currentUser } = this.props; //now we destructure our store state provide via our mapStateToProps and assign them to variables here
     if (authCheck) {
-      return loggedIn ? (
-        <div>
-          {currentUser.email}
-          <Logout />    
+      return loggedIn ? (  //seems to me all of our actual important links should be added in here
+        <div className="flex md:flex items-center w-full md:w-full">
+          <div className="order-1 md:order-2 md:w-auto md:flex items-center w-1/2 md:w-full">
+            <NavLink 
+              className="inline-block bg-gray-700 mx-1 px-4 py-3 rounded-md"
+              activeClassName="text-green-500"
+              to="/pets/new"
+              exact
+            >Add Pet</NavLink>
+            <NavLink 
+              className="inline-block bg-gray-700 mx-1 px-4 py-3 rounded-md"
+              activeClassName="text-green-500"
+              to="/owners/new"
+              exact
+            >Add Owner</NavLink>
+
+          </div> 
+          <div className="md:flex items-center order-3 md:order-1 ">
+            <Logout />    
+          </div>
+
         </div>
-      ) : (
-        <div>
-          <NavLink
-            className="inline-block bg-gray-700 px-4 py-3 rounded-md"
-            activeClassName="text-green-500"
-            to="/login"
-            exact>
-            Log In
-          </NavLink>
-          <NavLink
-            className="inline-block bg-gray-700 px-4 py-3 rounded-md"
-            activeClassName="text-green-500"
-            to="/signup"
-            exact>
-            Sign Up
-          </NavLink> 
+      ) : ( // then if they're not logged in they just see login or signup
+        <div className="order-1 md:order-3 w-full flex flex-wrap items-center justifyorder-end mr-0 md:mr-4">
+          <div className="auth md:flex items-center w-full md:w-full">
+              <NavLink
+              className="inline-block bg-transparent mx-1 text-gray-700 border border-gray-500 hover:bg-gray-700 hover:text-white px-4 py-3 rounded-md"
+              activeClassName="text-green-500"
+              to="/login"
+              exact>
+              Log In
+            </NavLink>
+            <NavLink
+              className="inline-block bg-transparent mx-1 text-gray-700 border border-gray-500 hover:bg-gray-700 hover:text-white px-4 py-3 rounded-md"
+              activeClassName="text-green-500"
+              to="/signup"
+              exact>
+              Sign Up
+            </NavLink> 
+          </div>
+          
         </div> 
       )
     } else {
@@ -48,45 +68,26 @@ class NavBar extends Component {
 
   render(){
     return(
-      <nav className="text-center bg-gray-200 text-white p-3 space-x-2">
-        <div>
-          <NavLink 
-              className="inline-block bg-gray-700 px-4 py-3 rounded-md"
-              activeClassName="text-green-500"
-              to="/"
-              exact
-            >Home</NavLink>
+      <div>
+        <nav className="flex w-full justify-between text-center bg-gray-200 text-white p-3 space-x-2">
+          <div className="md:flex md:items-center md:w-auto order-3 md:order-1">
             <NavLink 
-              className="inline-block bg-gray-700 px-4 py-3 rounded-md"
-              activeClassName="text-green-500"
-              to="/pets/new"
-              exact
-            >Add Pet</NavLink>
-            <NavLink 
-              className="inline-block bg-gray-700 px-4 py-3 rounded-md"
-              activeClassName="text-green-500"
-              to="/owners/"
-              exact
-            >Owners</NavLink>
-            <NavLink 
-              className="inline-block bg-gray-700 px-4 py-3 rounded-md"
-              activeClassName="text-green-500"
-              to="/owners/new"
-              exact
-            >Add Owner</NavLink>
-          </div>
-
-          <div>
-            <NavLink
-              className="inline-block bg-gray-700 px-4 py-3 rounded-md"
-              activeClassName="text-green-500"
-              to="/protected_route"
-              exact>
-              Protected Example
-            </NavLink>
-          </div>
-          <div>{this.renderAuthLinks()}</div> 
-      </nav>
+                className="inline-block bg-gray-700 mx-1 px-4 py-3  rounded-md"
+                activeClassName="text-green-500"
+                to="/"
+                exact
+              >Home</NavLink>
+              <NavLink 
+                className="inline-block bg-gray-700 mx-1 px-4 py-3 rounded-md"
+                activeClassName="text-green-500"
+                to="/owners/"
+                exact
+              >Owners</NavLink>
+            </div>
+            <div>{this.renderAuthLinks()}</div> 
+        </nav>
+      </div>
+      
     )
   }
 
--- pet-feeder-client/src/components/OwnerDeleteButton.js
+++ pet-feeder-client/src/components/OwnerDeleteButton.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { removeOwner } from '../actions/owners';
 
 
-class OwnerDeleteButton extends Component {
+const OwnerDeleteButton = () => {
 
-    state = {
-        status: ""
-    }
 
-    handleClick = () => {
+    const handleClick = () => {
         let setOwnerId = this.props.ownerId
         console.log("clicking this, Owner id =", setOwnerId)
         this.props.removeOwner(setOwnerId);
@@ -25,20 +22,13 @@ class OwnerDeleteButton extends Component {
 
     }
 
-    render() {
         return (
             <div className="md:px-2 mt-3 md:mt-0 items-center flex">
-                <button onClick={this.handleClick} className="bg-blue-500 text-white font-bold px-4 py-2 text-sm uppercase rounded tracking-wider focus:outline-none hover:bg-blue-600">DELETE OWNER</button>
+                <button onClick={handleClick} className="bg-blue-500 text-white font-bold px-4 py-2 text-sm uppercase rounded tracking-wider focus:outline-none hover:bg-blue-600">DELETE OWNER</button>
             </div>
         )
-    }
 }
 
-const mapStateToProps = (state) => {
-    return {
-
-    }
-}
 
 const mapDispatchToProps = (dispatch) => {
     return {
