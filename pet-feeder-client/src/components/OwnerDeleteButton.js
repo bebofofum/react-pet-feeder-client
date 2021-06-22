ddaa2e897aef10a2e1b1557c928e329bd475ec31
bebofofum <bri.boehm@gmail.com>
2021-06-21 22:41:27 -0400

    Made minor formatting changes for readability

--- pet-feeder-client/src/components/OwnerDeleteButton.js
+++ pet-feeder-client/src/components/OwnerDeleteButton.js
@@ -24,7 +24,11 @@ const OwnerDeleteButton = () => {
 
         return (
             <div className="md:px-2 mt-3 md:mt-0 items-center flex">
-                <button onClick={handleClick} className="bg-blue-500 text-white font-bold px-4 py-2 text-sm uppercase rounded tracking-wider focus:outline-none hover:bg-blue-600">DELETE OWNER</button>
+                <button 
+                onClick={handleClick} 
+                className="bg-blue-500 text-white font-bold px-4 py-2 text-sm uppercase rounded tracking-wider focus:outline-none hover:bg-blue-600">
+                DELETE OWNER
+                </button>
             </div>
         )
 }
